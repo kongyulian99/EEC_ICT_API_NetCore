af857be89391279124bbf71930218eb8643f568b
kongyulian99 <kongyulian94@163.com>
2025-06-16 07:55:20 +0700

    fix some bug

--- ApiService/Startup.cs
+++ ApiService/Startup.cs
@@ -35,7 +35,6 @@ namespace ApiService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            if(services.)
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowAngularClient", policy =>
